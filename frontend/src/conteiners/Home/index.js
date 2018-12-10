9ecee70f70c8817ac4f214d7443baf874c496a0b
mustafa1453 <ashustrich@gmail.com>
2018-12-10 16:49:37 +0200

    Fix jsx for home page

--- frontend/src/conteiners/Home/index.js
+++ frontend/src/conteiners/Home/index.js
@@ -4,20 +4,20 @@ import './Home.scss';
 class Home extends Component {
   render() {
     return (
-      <div class="wrapper">
-          <div class="vertical-center">
+      <div className="wrapper">
+          <div className="vertical-center">
               <h1>Welcome to freexit</h1>
               <h2>Online analog of dixit</h2>
-              <div class="loginform">
+              <div className="loginform">
                   <span>It's login and register form. If you are first at this site, just create your own username and password.</span>
-                  <div class="loginform__content">
-                      <label for="username">Username</label>
-                      <input type="text" name="username" class="loginform__user" />
+                  <div className="loginform__content">
+                      <label htmlFor="username">Username</label>
+                      <input type="text" name="username" className="loginform__user" />
 
-                      <label for="pass">Password</label>
-                      <input type="password" name="pass" class="loginform__pass" />
+                      <label htmlFor="pass">Password</label>
+                      <input type="password" name="pass" className="loginform__pass" />
 
-                      <button class="loginform__submit">Enter</button>
+                      <button className="loginform__submit">Enter</button>
                   </div>
               </div>
           </div>
