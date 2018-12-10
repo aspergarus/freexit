198b2329f0ba789d729559fa245c20834498b349
mustafa1453 <ashustrich@gmail.com>
2018-12-10 12:06:07 +0200

    Implemented design for login in react

--- frontend/src/conteiners/Home/index.js
+++ frontend/src/conteiners/Home/index.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
-import './Home.css';
+import './Home.scss';
 
 class Home extends Component {
   render() {
     return (
-      <h1>On the Home page</h1>
+      <div class="wrapper">
+          <div class="vertical-center">
+              <h1>Welcome to freexit</h1>
+              <h2>Online analog of dixit</h2>
+              <div class="loginform">
+                  <span>It's login and register form. If you are first at this site, just create your own username and password.</span>
+                  <div class="loginform__content">
+                      <label for="username">Username</label>
+                      <input type="text" name="username" class="loginform__user" />
+
+                      <label for="pass">Password</label>
+                      <input type="password" name="pass" class="loginform__pass" />
+
+                      <button class="loginform__submit">Enter</button>
+                  </div>
+              </div>
+          </div>
+      </div>
     );
   }
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import './index.css';
 import App from './conteiners/App';
-// import { HashRouter } from 'react-router-dom';
 // import * as serviceWorker from './serviceWorker';
 
-// render(<HashRouter><App/></HashRouter>, document.getElementById('root'));
 render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
